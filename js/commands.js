d4e3ce8277f8e56069c8115fa463ad36c3c3a0c7
DrKosmitek <60154600+DrKosmitek@users.noreply.github.com>
2023-03-06 07:01:03 +0100

    4

---
+++ js/commands.js
@@ -0,0 +1,23 @@
+help = [
+  "<br>",
+    'Sudo          <span class="imsobored">Admin Command (dont use if not admin)</span>',
+    'Help          <span class="imsobored">Gives a list of executable commands</span>',
+    'History       <span class="imsobored">Displays Command History</span>',
+    'Clear         <span class="imsobored">Clears the terminal</span>',
+    'Banner        <span class="imsobored">Displays the Mono Banner</span>',
+    'Radio         <span class="imsobored">Opens Racist Radio By Galaxy</span>',
+  "<br>",
+];
+
+
+
+banner = [
+  "    __  ___                ______                    _             __",
+  "   /  |/  /___  ____  ____/_  __/__  _________ ___  (_)___  ____ _/ /",
+  "  / /|_/ / __ \\/ __ \\/ __ \\/ / / _ \\/ ___/ __ `__ \\/ / __ \\/ __ `/ / ",
+  "  / /  / / /_/ / / / / /_/ / / /  __/ /  / / / / / / / / / / /_/ / /  ",
+  "/_/  /_/\\____/_/ /_/\\____/_/  \\___/_/  /_/ /_/ /_/_/_/ /_/\\__,_/_/   ",
+  "                                                             v1.0.2",
+  '<span class="color2">Welcome To The Mono Terminal Web Interface.</span>',
+  '<span class="color2">Type "help" for a list of executable commands',
+];
